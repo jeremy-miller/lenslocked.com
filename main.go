09a3466ae7a053b993e8920f73d28241fa2e0d97
Jeremy Miller <jeremymiller@fastmail.com>
2020-02-23 12:54:15 -0800

    Add 404 page

--- main.go
+++ main.go
@@ -17,5 +17,8 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprintf(w, "To get in touch, please email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "<h1>We could not find the page you were looking for :(</h1><p>Please email us if you keep being sent to an invalid page.</p>")
 	}
 }
