0dde37e069b2b56ac61c4a65f7c8846e5116fa90
Jeremy Miller <jeremymiller@fastmail.com>
2020-02-22 16:45:14 -0800

    Add chapter 2 code

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package lenslocked_com
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+
+	http.ListenAndServe(":3000", nil)
+}
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
+}
