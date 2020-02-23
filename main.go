0ee3bb80083d69b12c97cd2137c85c36ec24b8e3
Jeremy Miller <jeremymiller@fastmail.com>
2020-02-22 17:06:05 -0800

    Add content-type header

--- main.go
+++ main.go
@@ -12,5 +12,6 @@ func main() {
 }
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!</h1>")
 }
