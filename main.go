aebf3cff3290cba813abbebf049012a1ddfe6029
Jeremy Miller <jeremymiller@fastmail.com>
2020-02-23 15:09:31 -0800

    Add FAQ page

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
 
 	http.ListenAndServe(":3000", r)
 }
@@ -22,4 +23,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprintf(w, "To get in touch, please email <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>.")
+}
+
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprintf(w, "<h1>Frequently Asked Questions</h1><p>Here is a list of questions our users frequently ask.</p>")
 }
