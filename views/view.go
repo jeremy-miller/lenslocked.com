eafb222addd7877f7b41b275879e62e13f3d3031
Jeremy Miller <jeremymiller@fastmail.com>
2020-02-28 06:41:23 -0800

    Add globbing of layout files

--- views/view.go
+++ views/view.go
@@ -1,6 +1,14 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"path/filepath"
+)
+
+var (
+	layoutDir   = "views/layouts/"
+	templateExt = ".gohtml"
+)
 
 type View struct {
 	Template *template.Template
@@ -8,14 +16,19 @@ type View struct {
 }
 
 func New(layout string, files ...string) *View {
-	files = append(files,
-		"views/layouts/bootstrap.gohtml",
-		"views/layouts/navbar.gohtml",
-		"views/layouts/footer.gohtml",
-	)
+	files = append(files, layoutFiles()...)
 	t, err := template.ParseFiles(files...)
 	if err != nil {
 		panic(err)
 	}
 	return &View{Template: t, Layout: layout}
+}
+
+// layoutFiles returns a slice of strings representing the layout files used in our application.
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
 }
